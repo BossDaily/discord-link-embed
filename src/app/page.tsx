ed96365699203101835a657dfab60a3500d29de6
BossDaily <75937850+BossDaily@users.noreply.github.com>
2023-04-19 15:57:05 +0000

    boilerplate crap

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
+'use client'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import Embed from '@/Components/embed'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
-      
+      <p>hi</p>
+      <Embed />
     </main>
   )
 }
