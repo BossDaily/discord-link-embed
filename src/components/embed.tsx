1c76da85113cc7e90dcb41d5d56e5628f4027a7b
BossDaily <75937850+BossDaily@users.noreply.github.com>
2023-04-20 15:29:10 +0000

    changed folder name

--- src/components/embed.tsx
+++ src/components/embed.tsx
@@ -2,7 +2,12 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import { useForm, useWatch } from "react-hook-form";
-import { DiscordMessage, DiscordMessages } from "@skyra/discord-components-react";
+import {
+  DiscordEmbed,
+  DiscordEmbedDescription,
+  DiscordMessage,
+  DiscordMessages,
+} from "@skyra/discord-components-react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,13 +21,11 @@ export default function Embed() {
   const onSubmit = (data) => console.log(data);
   const fullData = watch();
 
-  const { epicData } = useWatch()
-
   console.log(fullData);
 
   return (
-    <div>
-      <form onSubmit={handleSubmit(onSubmit)}>
+    <div className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit(onSubmit)} className="grid grid-cols-2 gap-4">
         <input
           type="text"
           placeholder="Author"
@@ -31,7 +34,7 @@ export default function Embed() {
         <input
           type="url"
           placeholder="Author Icon URL"
-          {...register("Author Icon URL", { required: true, maxLength: 100 })}
+          {...register("Author_Icon_URL", { required: true, maxLength: 100 })}
         />
         <input
           type="text"
@@ -41,20 +44,26 @@ export default function Embed() {
         <input
           type="url"
           placeholder="Image URL"
-          {...register("Image URL", { required: false })}
+          {...register("Image_URL", { required: false })}
         />
         <input
           type="url"
           placeholder="Thumbnail URL"
-          {...register("Thumbnail URL", {})}
+          {...register("Thumbnail_URL", {})}
         />
         <input type="text" placeholder="Title" {...register("Title", {})} />
 
-        <input type="submit" />
       </form>
       <DiscordMessages>
-        <DiscordMessage>
-          {fullData.author}
+        <DiscordMessage
+          author="BossDaily"
+          avatar="https://cdn.discordapp.com/avatars/274973338676494347/00dcf84af54a0a58d2394b4054e0f7f5.png?size=100"
+        >
+          <DiscordEmbed authorName={fullData.Author} embedTitle={fullData.Title}>
+            <DiscordEmbedDescription slot="description">
+              {fullData.Description}
+            </DiscordEmbedDescription>
+          </DiscordEmbed>
         </DiscordMessage>
       </DiscordMessages>
     </div>
