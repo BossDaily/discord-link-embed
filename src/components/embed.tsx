6343cc6e970fd8e7b6a7bce7a0ca7fb1b140bbfc
BossDaily <75937850+BossDaily@users.noreply.github.com>
2023-04-25 15:41:45 +0000

    cleaned up css

--- src/components/embed.tsx
+++ src/components/embed.tsx
@@ -10,7 +10,13 @@ import {
 } from "@skyra/discord-components-react";
 import { HexColorPicker } from "react-colorful";
 import { useRouter, usePathname } from "next/navigation";
-import { useState } from "react";
+import { useState, ReactNode } from "react";
+import { Button } from "flowbite-react";
+import Link from "next/link";
+
+export function Header({ children }: { children: React.ReactNode }) {
+  return <h1 className="text-white text-6xl font-bold">{children}</h1>;
+}
 export default function Embed() {
   const {
     register,
@@ -37,69 +43,74 @@ export default function Embed() {
 
   const fullUrl = getFullUrl();
 
-  const url = `${fullUrl}?a=${fullData.Author}&au=${fullData.Author_Icon_URL}&d=${fullData.Description}&i=${fullData.Image_URL}&tu=${fullData.Thumbnail_URL}&t=${fullData.Title}&c=${color}`;
+  const url = `${fullUrl}/link?a=${fullData.Author}&au=${fullData.Author_Icon_URL}&d=${fullData.Description}&i=${fullData.Image_URL}&tu=${fullData.Thumbnail_URL}&t=${fullData.Title}&c=${color}`;
 
   return (
-    <div className="flex flex-col gap-4">
+    <div className="grid grid-cols-1 grow-0 gap-4">
       <form
         onSubmit={handleSubmit(onSubmit)}
-        className="grid grid-cols-2 gap-4 flex-none"
+        className="grid grid-cols-1 gap-4 md:grid-cols-2"
       >
         <input
           type="text"
           placeholder="Author"
           {...register("Author", { required: false, maxLength: 80 })}
-          className="bg-black rounded-xl hover:shadow-cornflower-blue-400 hover:shadow-xl text-white"
+          className="bg-black rounded-xl hover:shadow-cornflower-blue-400 hover:shadow-xl text-white max-h-10"
         />
         <input
           type="url"
           placeholder="Author Icon URL"
           {...register("Author_Icon_URL", { required: true, maxLength: 100 })}
-          className="bg-black rounded-xl hover:shadow-cornflower-blue-400 hover:shadow-xl text-white"
+          className="bg-black rounded-xl hover:shadow-cornflower-blue-400 hover:shadow-xl text-white max-h-10"
         />
-        <input
-          type="text"
+        <textarea
           placeholder="Description"
           {...register("Description", { required: false })}
-          className="bg-black rounded-xl hover:shadow-cornflower-blue-400 hover:shadow-xl text-white"
+          className=" bg-black rounded-xl hover:shadow-cornflower-blue-400 hover:shadow-xl text-white max-h-80"
         />
         <input
           type="url"
           placeholder="Image URL"
           {...register("Image_URL", { required: false })}
-          className="bg-black rounded-xl hover:shadow-cornflower-blue-400 hover:shadow-xl text-white"
+          className="bg-black rounded-xl hover:shadow-cornflower-blue-400 hover:shadow-xl text-white max-h-10"
         />
         <input
           type="url"
           placeholder="Thumbnail URL"
           {...register("Thumbnail_URL", {})}
-          className="bg-black rounded-xl hover:shadow-cornflower-blue-400 hover:shadow-xl text-white"
+          className="bg-black rounded-xl hover:shadow-cornflower-blue-400 hover:shadow-xl text-white max-h-10"
         />
         <input
           type="text"
           placeholder="Title"
           {...register("Title", {})}
-          className="bg-black rounded-xl hover:shadow-cornflower-blue-400 hover:shadow-xl text-white"
+          className="bg-black rounded-xl hover:shadow-cornflower-blue-400 hover:shadow-xl text-white max-h-10"
         />
       </form>
       <HexColorPicker color={color} onChange={setColor} />
-      <DiscordMessages className="rounded-xl">
-        <DiscordMessage
-          author="BossDaily"
-          avatar="https://cdn.discordapp.com/avatars/274973338676494347/00dcf84af54a0a58d2394b4054e0f7f5.png?size=100"
-        >
-          <DiscordEmbed
-            authorName={fullData.Author}
-            embedTitle={fullData.Title}
-            color={color}
+      <div className="">
+        <DiscordMessages className="rounded-xl grow-0 w-full">
+          <DiscordMessage
+            author="BossDaily"
+            avatar="https://cdn.discordapp.com/avatars/274973338676494347/00dcf84af54a0a58d2394b4054e0f7f5.png?size=100"
           >
-            <DiscordEmbedDescription slot="description">
-              {fullData.Description}
-            </DiscordEmbedDescription>
-          </DiscordEmbed>
-        </DiscordMessage>
-      </DiscordMessages>
-      <h2>{url.replaceAll(" ", "%20")}</h2>
+            <DiscordEmbed
+              authorName={fullData.Author}
+              embedTitle={fullData.Title}
+              color={color}
+            >
+              <DiscordEmbedDescription slot="description">
+                {fullData.Description}
+              </DiscordEmbedDescription>
+            </DiscordEmbed>
+          </DiscordMessage>
+        </DiscordMessages>
+      </div>
+      <Link href={url.replaceAll(" ", "%20")} className="justify-center">
+        <Button className="" style={{'backgroundColor': 'rgb(68 69 231)'}}>Go to URL</Button>
+      </Link>
+
+      {/* <h2>{url.replaceAll(" ", "%20")}</h2> */}
     </div>
   );
 }
