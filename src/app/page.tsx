a519b63e0247f8a10be1839d7c7218d6f3623045
BossDaily <75937850+BossDaily@users.noreply.github.com>
2023-04-25 15:41:38 +0000

    header text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import Embed from '@/components/embed'
+import Embed, { Header } from '@/components/embed'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>Create your own discord embed from a URL</p>
+
+      <Header>Create your own discord embed from a URL</Header>
       <Embed />
     </main>
   )
