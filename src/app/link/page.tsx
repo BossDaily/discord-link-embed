f1a10466da99e30fdc056d38b04d984bf5895239
BossDaily <75937850+BossDaily@users.noreply.github.com>
2023-04-27 14:47:41 +0000

    added metadata

--- src/app/link/page.tsx
+++ src/app/link/page.tsx
@@ -1,15 +1,34 @@
 import Preview from "@/components/preview";
-import { DiscordEmbed, DiscordEmbedDescription, DiscordMessage, DiscordMessages } from "@skyra/discord-components-react";
+import {
+  DiscordEmbed,
+  DiscordEmbedDescription,
+  DiscordMessage,
+  DiscordMessages,
+} from "@skyra/discord-components-react";
+import { Metadata } from "next";
 import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
+
+export async function generateMetadata(searchParams: {
+  [key: string]: string;
+}): Promise<Metadata> {
+  const embed: any = searchParams.searchParams
+  console.log(embed)
+  return {
+    title: embed.t,
+    openGraph: {
+      description: embed.d,
+    },
+    themeColor: embed.c,
+  };
+}
 
 export default function Page({
   params,
   searchParams,
 }: {
   params: { slug: string };
-  searchParams: { [key: string]: string  };
+  searchParams: { [key: string]: string };
 }) {
-  console.log(params, searchParams);
   return (
     <div>
       <Preview searchParams={searchParams} />
